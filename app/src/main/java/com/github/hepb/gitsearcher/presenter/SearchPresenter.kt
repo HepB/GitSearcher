384233010e511e55487a3928508fe31579fcf6e8
Alexandr Lyubimov <dr_kuvento@mail.ru>
2018-10-10 08:43:30 +0300

    Add rx, editText listener

--- app/src/main/java/com/github/hepb/gitsearcher/presenter/SearchPresenter.kt
+++ app/src/main/java/com/github/hepb/gitsearcher/presenter/SearchPresenter.kt
@@ -1,20 +1,30 @@
 package com.github.hepb.gitsearcher.presenter
 
+import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.github.hepb.gitsearcher.view.MvpSearchView
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 
 @InjectViewState
 class SearchPresenter : MvpPresenter<MvpSearchView>() {
-    private lateinit var userName: String
+    private val disposables: MutableList<Disposable> = mutableListOf()
 
     fun searchUser(userName: String) {
         //тут изо всех сил будем искать юзера
-        this.userName = userName
-        viewState.setFoundedUsers(this.userName)
     }
 
     fun listenSearchText(text: String) {
+        val single = Single.just(text)
+        val disposable = single.subscribe { result ->
+            Log.i("SearchPresenter", result)
+        }
+        disposables.add(disposable)
+    }
 
+    override fun onDestroy() {
+        disposables.forEach { it.dispose() }
+        super.onDestroy()
     }
 }
