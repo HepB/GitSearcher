cb4df71c1ea3342d9045a39ae428b3b1cef2d1e8
HepBo3 <dr_kuvento@mail.ru>
2018-10-18 15:42:53 +0500

    Add logging

--- app/src/main/java/com/github/hepb/gitsearcher/presenter/SearchPresenter.kt
+++ app/src/main/java/com/github/hepb/gitsearcher/presenter/SearchPresenter.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.util.Log
 import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
+import com.github.hepb.gitsearcher.data.model.view.SearchUserViewModel
 import com.github.hepb.gitsearcher.data.repo.search.UsersSearchRepo
 import com.github.hepb.gitsearcher.di.DaggerNetworkComponent
 import com.github.hepb.gitsearcher.view.MvpSearchView
@@ -14,38 +15,49 @@ import io.reactivex.subjects.PublishSubject
 @InjectViewState
 class SearchPresenter : MvpPresenter<MvpSearchView>() {
 
-    private var disposable: Disposable? = null
+    private val disposables: MutableList<Disposable> = mutableListOf()
+    private var users: MutableList<SearchUserViewModel> = mutableListOf()
     private lateinit var publishSubject: PublishSubject<String>
     private lateinit var usersSearchRepo: UsersSearchRepo
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
+        Log.i("SearchPresenter", "onFirstAttach")
         usersSearchRepo = DaggerNetworkComponent.builder().build().provideUsersSearchRepoImpl()
     }
 
-    @SuppressLint("CheckResult")
-    fun searchUser(userName: String) {
-        //тут изо всех сил будем искать юзера
-        usersSearchRepo.searchUsers(userName).subscribe{
-            result -> viewState.setFoundedUsers(result)
+    override fun attachView(view: MvpSearchView?) {
+        super.attachView(view)
+        Log.i("SearchPresenter", "onAttach")
+        if(users.isNotEmpty()) {
+            viewState.setFoundedUsers(users)
         }
     }
 
-    fun listenSearchText(text: String) {
-        if(disposable == null ) {
-            disposable = createSubscribe()
+    fun searchUser(userName: String) {
+        //тут изо всех сил будем искать юзера
+        val disposable = usersSearchRepo.searchUsers(userName).subscribe { result ->
+            users.addAll(result)
+            viewState.setFoundedUsers(result)
         }
+        disposables.add(disposable)
+    }
+
+    fun listenSearchText(text: String) {
+        val disposable = createSubscribe()
+        disposables.add(disposable)
         publishSubject.onNext(text)
     }
 
     override fun onDestroy() {
-        disposable?.dispose()
+        disposables.forEach { it.dispose() }
+        Log.i("SearchPresenter", "onDestroy")
         super.onDestroy()
     }
 
     private fun createSubscribe(): Disposable {
         publishSubject = PublishSubject.create()
-        return  publishSubject.filter { it.isNotEmpty() }.subscribe { result ->
+        return publishSubject.filter { it.isNotEmpty() }.subscribe { result ->
             Log.i("SearchPresenter", "PublishSubject $result")
         }
     }
